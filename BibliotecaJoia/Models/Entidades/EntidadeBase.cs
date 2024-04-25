88ef14624f66254228ad5b8f647a57be439dbc6e
fiedl <fiedl@DESKTOP-GNIO68V>
2024-04-25 16:28:18 -0300

    update projectAdd project files.

---
+++ BibliotecaJoia/Models/Entidades/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BibliotecaJoia.Models.Entidades
+{
+    public abstract class EntidadeBase
+    {
+        public string Id { get; set; }
+
+        public EntidadeBase() 
+        { 
+            Id = Guid.NewGuid().ToString(); 
+        }
+    }
+}
